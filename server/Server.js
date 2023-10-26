e5cd6d01aae2589576a13964a077b68d19b8069f
sherii190 <phonetech195@gmail.com>
2023-10-26 14:42:26 +0200

    database setup

--- server/Server.js
+++ server/Server.js
@@ -1,18 +1,22 @@
-const express = require ("express")
-const cors = require ("corse")
-const mongoose = require ("mongoose")
+const express = require("express");
+const cors = require("cors");
+const { mongoose } = require("mongoose");
 
-require("dotenv").config()
+require("dotenv").config();
 
-const app = express ()
-const PORT = process.env.PORT || 5000
+const app = express();
 
-//Middleware
-app.use(cors())
-app.use(express,json())
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 try {
-    
+  mongoose.connect(process.env.MONGODB_URI);
+  console.log("MongoDB connected...");
 } catch (error) {
-    
+  console.log(error);
 }
+
+app.listen(PORT, () => console.log(`Server is running at port: ${PORT}`));
