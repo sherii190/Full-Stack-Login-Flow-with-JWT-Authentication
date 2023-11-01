82a939036f3feef20d8d0ce45e28a7f08cbfa309
sherii190 <phonetech195@gmail.com>
2023-11-01 23:01:01 +0100

    space fix

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const { mongoose } = require("mongoose");
-
 const routes = require("./routes/authRoutes");
 
 require("dotenv").config();
